a927b4631405decedd8430b7eeb2dfa79a8a4642
Carlos <car.demarco@gmail.com>
2012-04-04 12:04:38 -0300

    Novo projeto LivroAndroidCap02
    Signed-off-by: Carlos <car.demarco@gmail.com>

---
+++ src/br/livro/android/cap2/PrimeiroExemplo.java
@@ -0,0 +1,12 @@
+package br.livro.android.cap2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PrimeiroExemplo extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
