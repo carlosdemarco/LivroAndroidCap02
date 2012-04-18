eb7f421357b32f5dc99133c384975c95a4509679
Carlos Augusto R. De Marco <car.demarco@gmail.com>
2012-04-18 18:07:32 -0300

    Inclusão de comentário Git
    Signed-off-by: Carlos Augusto R. De Marco <car.demarco@gmail.com>

--- src/br/livro/android/cap2/PrimeiroExemplo.java
+++ src/br/livro/android/cap2/PrimeiroExemplo.java
@@ -4,6 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class PrimeiroExemplo extends Activity {
+	
+	/**
+	 * Comentário Git
+	 */
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
