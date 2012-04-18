f5fed3e73479ac721c9c2ebc2f0dd6bee6bcbb15
Carlos Augusto R. De Marco <car.demarco@gmail.com>
2012-04-18 18:29:16 -0300

    Inclusão da classe SegundoExemplo.java
    Signed-off-by: Carlos Augusto R. De Marco <car.demarco@gmail.com>

---
+++ src/br/livro/android/cap2/SegundoExemplo.java
@@ -0,0 +1,15 @@
+package br.livro.android.cap2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SegundoExemplo extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+			
+	}
+
+}
